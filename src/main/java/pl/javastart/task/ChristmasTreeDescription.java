a230028b8ece30aaadb5cec7544fd59894a07fd5
Alina Burnos <burnosalina@gmail.com>
2023-01-06 15:17:54 +0100

    Opis dwoch choinek - update

--- src/main/java/pl/javastart/task/ChristmasTreeDescription.java
+++ src/main/java/pl/javastart/task/ChristmasTreeDescription.java
@@ -4,4 +4,9 @@ public class ChristmasTreeDescription {
     String treeColour;
     double treeHight;
     int christmasBallsNumber;
+    ChristmasTreeDescription(String treeColourN, double treeHightN, int christmasBallsNumberN){
+        treeColour = treeColourN;
+        treeHight = treeHightN;
+        christmasBallsNumber = christmasBallsNumberN;
+    }
 }
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -2,14 +2,8 @@ package pl.javastart.task;
 
 public class Main {
     public static void main(String[] args) {
-        ChristmasTreeDescription tree1 = new ChristmasTreeDescription();
-        tree1.treeColour = "Zielony";
-        tree1.treeHight = 1.80;
-        tree1.christmasBallsNumber = 158;
-        ChristmasTreeDescription tree2 = new ChristmasTreeDescription();
-        tree2.treeColour = "Zielono-bialy";
-        tree2.treeHight = 2.00;
-        tree2.christmasBallsNumber = 190;
+        ChristmasTreeDescription tree1 = new ChristmasTreeDescription("Zielony", 1.8, 158);
+        ChristmasTreeDescription tree2 = new ChristmasTreeDescription("Zielono-bialy", 2.00,190);
 
         System.out.println("Pierwsza choinka ma kolor:  " + tree1.treeColour + "\n" +
                 "Jej wysokosc to: " + tree1.treeHight + "m\n" + "Wisi na niej " + tree1.christmasBallsNumber +
