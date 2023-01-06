81a5864ab09535fd6c7288c5469fd35041a12dd6
Alina Burnos <burnosalina@gmail.com>
2023-01-06 15:49:28 +0100

    Opis dwoch choinek - update - new

---
+++ src/main/java/pl/javastart/task/ChristmasTree.java
@@ -0,0 +1,13 @@
+package pl.javastart.task;
+
+public class ChristmasTree {
+    String Colour;
+    double Height;
+    int christmasBallsNumber;
+
+    ChristmasTree(String treeColourN, double treeHightN, int christmasBallsNumberN) {
+        Colour = treeColourN;
+        Height = treeHightN;
+        christmasBallsNumber = christmasBallsNumberN;
+    }
+}
--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -2,15 +2,13 @@ package pl.javastart.task;
 
 public class Main {
     public static void main(String[] args) {
-        ChristmasTreeDescription tree1 = new ChristmasTreeDescription("Zielony", 1.8,
-                158);
-        ChristmasTreeDescription tree2 = new ChristmasTreeDescription("Zielono-bialy",
-                2.00, 190);
-        System.out.println("Pierwsza choinka ma kolor:  " + tree1.treeColour + "\n" +
-                "Jej wysokosc to: " + tree1.treeHight + "m\n" + "Wisi na niej " + tree1.christmasBallsNumber +
+        ChristmasTree tree1 = new ChristmasTree("Zielony", 1.8, 158);
+        ChristmasTree tree2 = new ChristmasTree("Zielono-bialy", 2.00, 190);
+        System.out.println("Pierwsza choinka ma kolor:  " + tree1.Colour + "\n" +
+                "Jej wysokosc to: " + tree1.Height + "m\n" + "Wisi na niej " + tree1.christmasBallsNumber +
                 " bombek.");
-        System.out.println("Druga choinka ma kolor:  " + tree2.treeColour + "\n" +
-                "Jej wysokosc to: " + tree2.treeHight + "m\n" + "Wisi na niej " + tree2.christmasBallsNumber +
+        System.out.println("Druga choinka ma kolor:  " + tree2.Colour + "\n" +
+                "Jej wysokosc to: " + tree2.Height + "m\n" + "Wisi na niej " + tree2.christmasBallsNumber +
                 " bombek.");
     }
 }
