5f5b4ef596887fa800bf1c92fb6f58966a11f77b
Alina Burnos <burnosalina@gmail.com>
2023-01-06 15:23:16 +0100

    Opis dwoch choinek - update

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -2,8 +2,10 @@ package pl.javastart.task;
 
 public class Main {
     public static void main(String[] args) {
-        ChristmasTreeDescription tree1 = new ChristmasTreeDescription("Zielony", 1.8, 158);
-        ChristmasTreeDescription tree2 = new ChristmasTreeDescription("Zielono-bialy", 2.00,190);
+        ChristmasTreeDescription tree1 = new ChristmasTreeDescription("Zielony", 1.8,
+                158);
+        ChristmasTreeDescription tree2 = new ChristmasTreeDescription("Zielono-bialy",
+                2.00,190);
 
         System.out.println("Pierwsza choinka ma kolor:  " + tree1.treeColour + "\n" +
                 "Jej wysokosc to: " + tree1.treeHight + "m\n" + "Wisi na niej " + tree1.christmasBallsNumber +
